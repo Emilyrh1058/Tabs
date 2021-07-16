eae53d86a7ff9aab708ef8b934d55e46cdb053cc
Emily Salinas <emilyrh1058@gmail.com>
2021-07-15 19:39:35 -0500

    .

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
