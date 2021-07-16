67c5019335883218fbe685d1c1b4d0f12c5c3a9b
cliffybar <crowellclifford@utexas.edu>
2021-07-15 20:23:13 -0500

    video background not working. changed package.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 //import logo from './logo.svg';
 import './App.css';
+import './assets/backgroundVideo/index';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
