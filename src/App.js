9ccc715133da8e87eb7999af2902073254be4a01
Terry Theis <tltheis@prodigy.net>
2021-07-15 20:23:09 -0500

    merge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
-import backgroundVideo from './assets/backgroundVideo/index';
+//import backgroundVideo from './assets/backgroundVideo/index';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-       <backgroundVideo></backgroundVideo>
-        {/* <p>
-=======
       
         <p>
->>>>>>> eae53d86a7ff9aab708ef8b934d55e46cdb053cc
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -22,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
+        </a>
       </header>
     </div>
   );
