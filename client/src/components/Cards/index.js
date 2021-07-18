c93d0ffff08fb370e3cf2e4226d4d27ffd7e8ade
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-07-18 13:50:48 -0500

    routes fixed and added to app.js

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Nav from '../Nav';
-import Cards from './Cards';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Background from "../Background";
+import Home from "../Home";
+import Nav from "../Nav";
+import Cards from "./Cards";
 
 const cards = () => (
-    <body>
-        <Switch>
-            <Route exact path='/contacts' component={Cards} />
-            <Route exact path='/contacts' component={Nav} />
-        </Switch>
-    </body>
-)
+  <body>
+    <Switch>
+      <Route exact path="/" component={Background} />
+      {/* <Route exact path="/" component={Home} /> */}
+      <Route exact path="/contacts" component={Cards} />
+      <Route exact path="/contacts" component={Nav} />
+    </Switch>
+  </body>
+);
 
 export default Cards;
