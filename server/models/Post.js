62540bf5b6d5fe79f622f57830bd5c6f37704acd
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-07-18 11:54:35 -0500

    graphql refactored

--- server/models/Post.js
+++ server/models/Post.js
@@ -9,19 +9,6 @@ const postSchema = new Schema({
   phone: String,
   username: String,
   createdAt: String,
-  comments: [
-    {
-      body: String,
-      username: String,
-      createdAt: String,
-    },
-  ],
-  likes: [
-    {
-      username: String,
-      createdAt: String,
-    },
-  ],
   user: {
     type: Schema.Types.ObjectId,
     ref: "users",
