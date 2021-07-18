0c42f585bd668b5e29452b674bbf6d961a1589ac
Nancy <nancyrosas0@gmail.com>
2021-07-17 21:26:59 -0500

    took out nav on contacts page

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Nav from '../Nav';
 import Cards from './Cards';
 
 const cards = () => (
     <body>
         <Switch>
             <Route exact path='/contacts' component={Cards} />
+            <Route exact path='/contacts' component={Nav} />
         </Switch>
     </body>
 )
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Auth from "../../utils/auth";
+//import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
 function Nav() {
