71a35d03386b9459a725b162f7052a8d29949ff2
Terry Theis <tltheis@prodigy.net>
2021-07-18 10:13:18 -0500

    Ter merge

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
-=======
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
->>>>>>> develop
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
