c58343d9f0f946bdf6a1a82cf49b1751ce0485a9
Emily Salinas <emilyrh1058@gmail.com>
2021-07-17 20:41:35 -0500

    changed all thoughts to notes

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1>Deep Thoughts</h1>
+        <h1>TABS</h1>
       </div>
     </header>
   );
