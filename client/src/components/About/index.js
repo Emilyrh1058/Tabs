837304f6644b001bdf72261475978246dabe5052
cliffybar <crowellclifford@utexas.edu>
2021-07-21 13:32:17 -0500

    initialize new branch

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
     <section className="my-5">
       <div className="container d-flex flex-column text-center">
         <div className="p-2">
-          <h1 id="about">TABS</h1>
+          <h1 id="about">T A B S</h1>
         </div>
         <div className="p-2">
           <p className="aboutMe">
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -12,7 +12,7 @@ const Login = () => {
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
-          <h1>TABS</h1>
+          <h1>T A B S</h1>
         </Link>
 
         <nav className="text-center">
@@ -25,8 +25,8 @@ const Login = () => {
             </>
           ) : (
             <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
+              <Link to="/login">LOGIN</Link>
+              <Link to="/signup">CREATE</Link>
             </>
           )}
         </nav>
