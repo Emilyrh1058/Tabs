ab520b5943b31d5925fceafc06cb378ebe249499
Terry Theis <tltheis@prodigy.net>
2021-07-20 20:32:09 -0500

    merge dev

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,7 +1,8 @@
 // import React, { useState, useEffect } from "react";
 import cardData from "../../utils/cardData";
 import { Card, ListGroup, ListGroupItem, Row, Col } from "react-bootstrap";
-
+import { Route, Switch } from "react-router-dom";
+import Nav from "../Nav";
 const Cards = () => (
     <body>
         <Switch>
