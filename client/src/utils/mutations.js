323de736265e03e977cb18db36bda19b7d1de82c
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-07-20 20:01:22 -0500

    mutations added to front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
+// Confirm if its being used:
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -12,9 +13,39 @@ export const LOGIN_USER = gql`
   }
 `;
 
+// Confirm if its being used:
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// create post
+
+export const createPost = gql`
+  mutation createPost(
+    $firstName: String!
+    $lastName: String!
+    $age: String!
+    $city: String!
+    $state: String!
+    $phone: String!
+  ) {
+    createPost(
+      firstName: $firstName
+      lastName: $lastName
+      age: $age
+      city: $city
+      state: $state
+      phone: $phone
+    ) {
       token
       user {
         _id
