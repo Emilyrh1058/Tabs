61392d356b3f3f48ddf03d27f3d8b5799c62950c
Emily Salinas <emilyrh1058@gmail.com>
2021-07-21 21:01:52 -0500

    ..

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
-const {typeDefs, resolvers} = require('./schemas');
+const typeDefs = require('./schemas/typeDefs');
+const resolvers = require('./schemas/resolvers');
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
