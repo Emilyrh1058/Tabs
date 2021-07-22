6b7955452409e452fe7d44dcec7126cc799e1353
cliffybar <crowellclifford@utexas.edu>
2021-07-22 03:23:37 -0500

    changed className to login-header and applied all CSS stylings to new class

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -42,7 +42,7 @@ const Login = (props) => {
   return (
     <main className='flex-row justify-center mb-4'>
       <div className='col-12 col-md-6'>
-        <div className='card'>
+        <div className='homepage-card'>
           <h4 className='login-header'>Login</h4>
           <div className='card-body'>
             <form onSubmit={handleFormSubmit}>
