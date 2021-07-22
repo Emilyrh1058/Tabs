1745494415fb75857ead0dc9d9ed1b2e9f1b1f0f
cliffybar <crowellclifford@utexas.edu>
2021-07-22 03:28:07 -0500

    added login-fail status styling

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -43,7 +43,7 @@ const Login = (props) => {
     <main className='flex-row justify-center mb-4'>
       <div className='col-12 col-md-6'>
         <div className='homepage-card'>
-          <h4 className='login-header'>Login</h4>
+          <h4 className='login-word'>Login</h4>
           <div className='card-body'>
             <form onSubmit={handleFormSubmit}>
               <input
@@ -68,7 +68,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
-            {error && <div>Login failed</div>}
+            {error && <div className='login-fail'>Login failed</div>}
           </div>
         </div>
       </div>
