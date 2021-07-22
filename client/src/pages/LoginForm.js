e732d6ccbe68dafcd964ddda1c2e32bb4ade991e
cliffybar <crowellclifford@utexas.edu>
2021-07-22 03:16:14 -0500

    change homepage login card label color to white

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -43,7 +43,7 @@ const Login = (props) => {
     <main className='flex-row justify-center mb-4'>
       <div className='col-12 col-md-6'>
         <div className='card'>
-          <h4 className='card-header'>Login</h4>
+          <h4 className='login-header'>Login</h4>
           <div className='card-body'>
             <form onSubmit={handleFormSubmit}>
               <input
