bc5ce66ae5964af20da9ce487d6d0620a898e559
Nancy <nancyrosas0@gmail.com>
2021-07-22 16:44:03 -0500

    added icons fire, ice and rose photo

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,6 +1,7 @@
 // import React, { useState, useEffect } from "react";
 import cardData from "../../utils/cardData";
 import { Card, ListGroup, ListGroupItem, Row, Col } from "react-bootstrap";
+import rose from '../../assets/images/rose.png'
 /**
  *  create a post request to backend to get cards
  *  once data is received dinamically display cards
@@ -16,7 +17,7 @@ function Cards(props) {
       {cardData.map((data, idx) => (
         <Col>
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Img variant="top" src={rose}/>
             <Card.Body>
               <Card.Title>
                 {data.firstName} {data.lastName}
@@ -32,8 +33,8 @@ function Cards(props) {
               <ListGroupItem> {data.phoneNumber} </ListGroupItem>
             </ListGroup>
             <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <Card.Img src="https://image.flaticon.com/icons/png/512/907/907838.png" width="50" height= "50" hspace="10"></Card.Img>
+              <Card.Img src="https://image.flaticon.com/icons/png/512/1160/1160990.png" width="50" height= "50" hspace="10"></Card.Img>
             </Card.Body>
           </Card>
         </Col>
